5b8a640ee21d698a6fc936bb6e0b885676624c8e
PatriceVignola <vignola.patrice@gmail.com>
2018-05-28 13:01:24 +0900

    Fix the exported `dotModel` flow type

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ export type DotModel = {
   uvCoordinates: number[],
   normalCoordinates: number[],
   numVerticesPerFace: number,
+  numCoordinatesPerVertex: number,
 };
 
 function parseIndices(streamReader: StreamReader): number[] {
